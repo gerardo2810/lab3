4320cfb88f86b922fe8f822a708f4317c68b493b
gerardo2810 <144623841+gerardo2810@users.noreply.github.com>
2024-04-10 21:54:53 -0500

    patos

--- src/main/java/com/example/demo/jue.java
+++ src/main/java/com/example/demo/jue.java
@@ -2,23 +2,29 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
 public class jue {
-    @Controller
-    public class juego {
-        @GetMapping("/patito_hule")
-        public String patitos() {
+    /*@GetMapping("/patito_hule"){
+        public String patitos(@RequestParam("fila") String fila,
+                              @RequestParam("columna") String columna,
+                              @RequestParam("iniciales") String inicales,
+                              @RequestParam("fotos") String columnas) {
             return "/patito_hule";
         }
     }
 
-    @Controller
+    @GetMapping("/patito_hule/play")
     public class play {
         @GetMapping("/patito_hule/play")
         public String patitos1() {
             return "/patito_hule/play";
         }
+    }*/
+    @GetMapping("/patito_hule")
+    public String patitos() {
+        return "/patito_hule";
     }
-
 
 }
